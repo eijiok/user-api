b5effb2f75dbf9d5a294ccdb4fc32a78fef76207
Eiji Okuda <fabio.okuda@loggi.com>
2022-12-15 23:48:56 -0300

    add gorilla/mux lib

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
 func main() {
-	print("Hello world!")
+	router := mux.NewRouter()
+
+	router.
+		PathPrefix("/api").
+		Path("/users").
+		Methods(http.MethodGet).
+		HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+			writer.WriteHeader(http.StatusOK)
+			writer.Write([]byte("testing"))
+		})
+
+	err := http.ListenAndServe(":8080", router)
+	if err != nil {
+		log.Fatalf("Error on router initialization: %s", err.Error())
+	}
 }
